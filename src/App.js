db095ec44a05441d2e157bf52eaa060b6a6bf204
Alex <Alex@Users-MacBook-Pro-2.local>
2022-04-29 10:05:42 +0200

    first

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
+import React, {useState, useCallback} from 'react';
+
+import Button from './components/UI/Button/Button'
 import './App.css';
+import DemoOutput from './components/Demo/DemoOutput';
 
 function App() {
+  const [showParagraph, setShowParagraph] = useState(false);
+  const [allowToggle, setAllowToggle] = useState(false);
+
+  console.log('app running');
+
+  const showParagraphHandler = useCallback(() => {
+    if (allowToggle) {
+      setShowParagraph((prevShowParagraph) => ! prevShowParagraph);
+    }
+  }, [allowToggle]);
+
+  const allowToggleHandler = () => {
+    setAllowToggle(true)
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <h1>Hi there!</h1>
+      <DemoOutput show={showParagraph} />
+      <Button onClick={allowToggleHandler}>allow toggle</Button>
+      <Button onClick={showParagraphHandler}>show paragraph</Button>
     </div>
   );
 }
---
+++ src/components/Demo/DemoOutput.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+import MyParagraph from "./MyParagraph";
+
+const DemoOutput = (props) => {
+    console.log('demo running')
+    return <MyParagraph>{props.show ? 'this is new' : ''}</MyParagraph>;
+};
+
+export default React.memo(DemoOutput);
